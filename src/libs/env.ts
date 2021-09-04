04038199b98d998dca6901bdb4aec3c27edc4be4
hoonta24 <kadr8941@gmail.com>
2021-09-04 22:18:33 +0900

    Add the deploy workflow for github actions

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -1,6 +1,6 @@
 const env = {
   BASE_PATH: process.env.BASE_PATH || "",
-  ARTICLES_ROOT: process.env.ARTICLES_ROOT || "_posts",
+  ARTICLES_ROOT: process.env.ARTICLES_ROOT || ".articles",
   ARTICLE_FILENAME: process.env.ARTICLE_FILENAME || "README.md",
 } as const
 
