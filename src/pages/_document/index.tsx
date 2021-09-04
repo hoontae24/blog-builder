7d9ae85c9c1f2d1abcf69572459aba2c9e7c1622
hoonta24 <kadr8941@gmail.com>
2021-09-04 13:45:00 +0900

    Add the link for fontawesome (#5)

--- src/pages/_document/index.tsx
+++ src/pages/_document/index.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* google font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
@@ -24,6 +25,14 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
+          />
+
+          {/* fontawesome */}
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
