d5710116e23a7d053214a6d85761996dcb11bba2
TAEHOON <kadr8941@gmail.com>
2021-09-10 10:00:32 +0900

    Update env.ts

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -3,7 +3,7 @@ const env = {
   ARTICLES_ROOT: process.env.ARTICLES_ROOT || ".articles",
   ARTICLE_FILENAME: process.env.ARTICLE_FILENAME || "README.md",
   BLOG_TITLE: process.env.BLOG_TITLE || "_BLOG",
-  GITHUB_URL: process.env.GITHUB_URL,
+  GITHUB_URL: process.env.GH_URL,
 } as const
 
 export default env
