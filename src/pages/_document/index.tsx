7f79388cd71cff1f6a4318fd786ab453e2a3636c
TAEHOON <kadr8941@gmail.com>
2021-09-15 10:26:53 +0900

    Add the lang attribute for the html document
    HTML 문서 lang 속성을 "ko" 값으로 지정

--- src/pages/_document/index.tsx
+++ src/pages/_document/index.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="ko">
         <Head>
           {/* google font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
