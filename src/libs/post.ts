57cec3861690eafaa468ff311ec56df5a7476af6
TAEHOON <kadr8941@gmail.com>
2021-09-30 15:27:39 +0900

    Change post date rendering format

--- src/libs/post.ts
+++ src/libs/post.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs"
 
 export const formatPostDate = (date: string | Date): string => {
-  return dayjs(date).format("YYYY. M. D.")
+  return dayjs(date).format("YYYY.MM.DD")
 }
