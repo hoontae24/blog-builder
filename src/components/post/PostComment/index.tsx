15862a150f8471eccf6d0875f78d3d9456cafcae
TAEHOON <kadr8941@gmail.com>
2021-10-06 21:55:17 +0900

    Fix utterances for Post Comment (#26)
    * Dismiss post comment
    
    * Update index.tsx
    
    * Fix the utterances appending logic

--- src/components/post/PostComment/index.tsx
+++ src/components/post/PostComment/index.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx"
+import { useEffect, useRef } from "react"
 
 import config from "@/config.json"
 import { HasClassName } from "@/types/component"
@@ -15,24 +16,36 @@ type Props = OwnProps
 const _PostComment = (props: Props) => {
   const { className, post } = props
   const term = [post.category, post.slug].join("/")
-  return (
-    <div
-      className={clsx(cls.root, className)}
-      dangerouslySetInnerHTML={{
-        __html: [
-          `<script`,
-          `  src="https://utteranc.es/client.js"`,
-          `  repo="${config.utterances.repo}"`,
-          `  issue-term="${term}"`,
-          `  label="${config.utterances.label}"`,
-          `  theme="${config.utterances.theme}"`,
-          `  crossorigin="anonymous"`,
-          `  async`,
-          `/>`,
-        ].join("\n"),
-      }}
-    />
-  )
+
+  const rootRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (typeof document !== "undefined" && rootRef.current) {
+      const attrs = {
+        src: "https://utteranc.es/client.js",
+        repo: config.utterances.repo,
+        "issue-term": term,
+        label: config.utterances.label,
+        theme: config.utterances.theme,
+        crossOrigin: "anonymous",
+        async: "true",
+      }
+
+      const script = document.createElement("script")
+
+      Object.entries(attrs).forEach(([key, value]) => {
+        script.setAttribute(key, value)
+      })
+
+      rootRef.current.appendChild(script)
+
+      return () => {
+        rootRef.current?.removeChild(script)
+      }
+    }
+  }, [term])
+
+  return <div className={clsx(cls.root, className)} ref={rootRef} />
 }
 
 const PostComment = _PostComment
